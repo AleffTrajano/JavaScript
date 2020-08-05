cfc0ce5e7e42b85ea2cdfc8430e01afaa7eea4c8
unknown <aleff_diniz@hotmail.com>
2020-08-04 21:42:08 -0300

    revisao 1

---
+++ js/main.js
@@ -0,0 +1,59 @@
+
+//data
+var d = new Date();
+//alert (d.getDay)
+alert (d.getHours());
+//alert (d.getMonth)
+
+
+//repetição com for
+
+var count;
+for(count=0; count<=10; count++){
+    alert(count)
+}
+
+
+
+/*repetição
+var count = 1;
+while (count <= 5){
+    console.log(count),
+    alert(count),
+    count++;
+    
+};
+*/
+
+/*condicional
+
+var idade = prompt("Qual sua idade");
+if(idade>=20){
+    alert("fodao")
+}else{
+    alert("fodinha")
+}
+*/
+
+
+/*var nome ="Aleff Diniz";
+var funcao= "Software Devellop";
+alert( "A funcao de :" + nome+" é:"+funcao)
+console.log(nome)
+console.log(funcao)
+var list= ["java","Angular","JavaScript"]
+*/
+/*list.push("php")
+//list.pop("cobol")
+//list.log(list.length);
+console.log(list)
+//console.log(list.toString());
+console.log(list.join("-"))
+alert(list[1]);
+*/
+
+/*criando dicionario
+//var fruta={nome:"goiaba",cor:"verde"}
+//console.log(fruta.nome)
+alert (fruta.cor)
+*/
